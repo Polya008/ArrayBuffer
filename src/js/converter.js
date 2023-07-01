43aa68860806c7e34856738fa226c73d8aa1c0ad
Екатерина <polskaya007@mail.ru>
2023-07-01 15:38:58 +0300

    version 3

--- src/js/converter.js
+++ src/js/converter.js
@@ -1,4 +1,4 @@
-export function getBuffer() {
+export default function getBuffer() {
   const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
   return ((input) => {
     const buffer = new ArrayBuffer(data.length * 2);
